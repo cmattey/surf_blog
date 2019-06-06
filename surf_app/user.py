0caa1c9a795b1ef887aced1d3f61ee40c54c9642
cmattey <cmattey@dons.usfca.edu>
2019-06-05 21:13:43 -0700

    --*done*-- added follow functionality for users

--- surf_app/user.py
+++ surf_app/user.py
@@ -19,25 +19,62 @@ def user_profile(username):
     ' ORDER BY created DESC',(user['id'],)
     ).fetchall()
 
-    return render_template('user/user_profile.html', user=user, posts=user_posts)
+    return render_template('user/user_profile.html', user=user, posts=user_posts,is_following=is_following(username))
 
 @bp.route('/follow/<username>')
 @login_required
 def follow(username):
     db = get_db()
-    error = None
     current_user = g.user
 
     follow_user = db.execute('SELECT * FROM user WHERE username = (?)',(username,)).fetchone()
 
     if follow_user is None:
+        flash('User {} does not exist'.format(username))
+        return redirect(url_for('blog.index'))
+    elif follow_user['id']==session['user_id']:
+        flash('You can Follow yourself')
+        return redirect(url_for('blog.index'))
+    elif is_following(username):
+        flash('You are already following {}.format(username)')
+        return redirect(url_for('user.user_profile',username=username))
 
+    db.execute('INSERT INTO user_relations (follower_id, followed_id) VALUES (?,?)',
+            (session['user_id'],follow_user['id']))
 
-    db.execute('')
-
-    pass
+    db.commit()
+    flash("You have succesfully followed {}".format(username))
+    return redirect(url_for('user.user_profile',username=username))
 
 @bp.route('/unfollow/<username>')
 @login_required
 def unfollow(username):
-    pass
+    db = get_db()
+    error = None
+    current_user = g.user
+
+    unfollow_user = db.execute('SELECT * FROM user WHERE username = (?)',(username,)).fetchone()
+
+    if unfollow_user is None:
+        flash('User {} does not exist'.format(username))
+        return redirect(url_for('blog.index'))
+
+    db.execute('DELETE FROM user_relations WHERE follower_id=(?) and followed_id=(?)',
+            (session['user_id'],unfollow_user['id']))
+
+    db.commit()
+    flash("You have succesfully unfollowed {}".format(username))
+    return redirect(url_for('user.user_profile',username=username))
+
+def is_following(username):
+    db = get_db()
+    follow_user = db.execute('SELECT * FROM user WHERE username = (?)',(username,)).fetchone()
+    follow_row = db.execute('SELECT * FROM user_relations WHERE follower_id=(?) and followed_id=(?)',
+            (session['user_id'],follow_user['id'])).fetchone()
+
+    if follow_user is None:
+        return False
+    elif follow_row is not None:
+        return True
+
+    return False
